5a891d8262ce8f3ed8f91d246b3fa67398ac4074
JasonLei <jlei@activision.com>
2024-02-05 09:58:39 -0800

    clean up files

---
+++ shopcart/src/Products.js
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: 0,
+    name: "Unisex Cologne",
+    image: "./products/cologne.jpg",
+    quantity: 0,
+    rating: "4.5/5",
+  },
+  {
+    id: 1,
+    name: "Apple iWatch",
+    image: "./products/iwatch.jpg",
+    quantity: 0,
+    rating: "4.6/5",
+  },
+  {
+    id: 2,
+    name: "Unique Mug",
+    image: "./products/mug.jpg",
+    quantity: 0,
+    rating: "4.8/5",
+  },
+  {
+    id: 3,
+    name: "Mens Wallet",
+    image: "./products/wallet.jpg",
+    quantity: 0,
+    rating: "4.9/5",
+  },
+];
